966d447506bcec0ef887e9f558e3590d43236f70
KalminX <noriabure@gmail.com>
2025-12-05 08:29:35 +0100

    made updates

--- app/utils.py
+++ app/utils.py
@@ -7,6 +7,6 @@ def verify_password(plain_password, hashed_password):
     """Function to verify the plain password against the hashed password"""
     return pwd_context.verify(plain_password, hashed_password)
 
-def get_password_hash(password):
-    """Function to hash the plain password using bcrypt algorithm"""
-    return pwd_context.hash(password)
+def get_password_hash(password: str):
+    return pwd_context.hash(password[:72])
+
